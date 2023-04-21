9213a9464d7deb056da1d2992110b81a0a70e567
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-21 17:00:36 +0200

    Add more tests for `EmptyUndirectedGraph`

--- src/test/kotlin/io/github/alexandrepiveteau/graphs/UndirectedGraph.complete.kt
+++ src/test/kotlin/io/github/alexandrepiveteau/graphs/UndirectedGraph.complete.kt
@@ -3,13 +3,21 @@ package io.github.alexandrepiveteau.graphs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
 
 class UndirectedEmptyGraphTests {
 
   @Test
-  fun `empty graph has no vertices`() {
+  fun `empty graph has no vertices nor edges`() {
     val graph = UndirectedGraph.empty()
+
     assertEquals(0, graph.size)
+    assertFalse(graph.contains(Vertex(0)))
+    assertFalse(graph.contains(Edge(Vertex(0), Vertex(1))))
+    assertFailsWith<IndexOutOfBoundsException> { graph[0] }
+    assertFailsWith<NoSuchVertexException> { graph[Vertex(0)] }
+    assertFailsWith<IndexOutOfBoundsException> { graph.neighborsSize(0) }
+    assertFailsWith<IndexOutOfBoundsException> { graph.neighbor(0, 0) }
   }
 }
 
