c538d094d69c2deacf8d0d8f745a42829800accd
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-21 16:57:16 +0200

    Add some tests for BFS on undirected graphs

--- src/test/kotlin/io/github/alexandrepiveteau/graphs/UndirectedGraph.traversals.kt
+++ src/test/kotlin/io/github/alexandrepiveteau/graphs/UndirectedGraph.traversals.kt
@@ -6,6 +6,31 @@ import kotlin.test.assertEquals
 class UndirectedGraphDFSTests {
 
   @Test
+  fun `bfs on singleton graph visits one vertex`() {
+    val graph = UndirectedGraph.complete(1)
+    var visited = 0
+    graph.forEachVertexBreadthFirst(graph[0]) { visited++ }
+    assertEquals(1, visited)
+  }
+
+  @Test
+  fun `bfs on line graph visits all vertices`() {
+    for (count in 1..Repeats) {
+      val graph = buildUndirectedGraph {
+        var last = addVertex()
+        for (i in 1 until count) {
+          val next = addVertex()
+          addEdge(Edge(last, next))
+          last = next
+        }
+      }
+      val visited = BooleanArray(count)
+      graph.forEachVertexBreadthFirst(graph[0]) { visited[it.index] = true }
+      assertEquals(true, visited.all { it })
+    }
+  }
+
+  @Test
   fun `dfs on singleton graph visits one vertex`() {
     val graph = UndirectedGraph.complete(1)
     var visited = 0
