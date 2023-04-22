b5b5da04f8daafcd9113e797a228eb371e4a32a4
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-22 23:24:49 +0200

    Add some tests for `Arc`

---
+++ src/test/kotlin/io/github/alexandrepiveteau/graphs/Arc.kt
@@ -0,0 +1,37 @@
+package io.github.alexandrepiveteau.graphs
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ArcTests {
+
+  @Test
+  fun `arcs are properly reversed`() {
+    val a = Vertex(0)
+    val b = Vertex(1)
+    val e = a arcTo b
+    val r = e.reversed()
+    assertEquals(a, r.to)
+    assertEquals(b, r.from)
+  }
+
+  @Test
+  fun `arc contains both extremities`() {
+    val a = Vertex(0)
+    val b = Vertex(1)
+    val e = a arcTo b
+    assertTrue(a in e)
+    assertTrue(b in e)
+  }
+
+  @Test
+  fun `arc destructuring returns extremities in order`() {
+    val a = Vertex(0)
+    val b = Vertex(1)
+    val e = a arcTo b
+    val (from, to) = e
+    assertEquals(a, from)
+    assertEquals(b, to)
+  }
+}
