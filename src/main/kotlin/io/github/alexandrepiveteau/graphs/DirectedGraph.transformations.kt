48b659013cef1a64eb45197e2454f777ee854cda
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-23 12:19:53 +0200

    Add `toUndirectedGraph()` and `toDirectedGraph()` extensions

--- src/main/kotlin/io/github/alexandrepiveteau/graphs/DirectedGraph.transformations.kt
+++ src/main/kotlin/io/github/alexandrepiveteau/graphs/DirectedGraph.transformations.kt
@@ -1,5 +1,14 @@
 package io.github.alexandrepiveteau.graphs
 
+/**
+ * Transforms the [DirectedGraph] into an [UndirectedGraph], by adding an edge between each pair of
+ * vertices that are connected by an arc.
+ */
+public fun DirectedGraph.toUndirectedGraph(): UndirectedGraph = buildUndirectedGraph {
+  forEachVertex { addVertex() }
+  forEachArc { (u, v) -> addEdge(u edgeTo v) }
+}
+
 /** Returns the transposed [DirectedGraph], where the direction of arcs has been reversed. */
 public fun DirectedGraph.transposed(): DirectedGraph = buildDirectedGraph {
   forEachVertex { addVertex() }
