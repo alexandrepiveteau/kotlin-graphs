3c76ba7c523308f9864935f59b1101150f5c9a08
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-24 15:14:12 +0200

    Start setting up deployment through Maven Central

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+## [Unreleased]
+
+## [0.1.0] - 2023-04-24
+
+Initial release
+
+
+[Unreleased]: https://github.com/cashapp/turbine/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/alexandrepiveteau/kotlin-graphs/releases/tag/0.1.0
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,35 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
   kotlin("multiplatform") version "1.8.20"
   id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.0"
   id("org.jetbrains.dokka") version "1.8.10"
+  id("com.vanniktech.maven.publish") version "0.25.2"
 }
 
 repositories { mavenCentral() }
 
 kotlin {
   explicitApi()
-  jvm()
+
   js(IR) {
-    browser()
     nodejs()
+    browser()
   }
-  ios()
-  macosArm64()
-  macosX64()
-  linuxArm64()
+
+  jvm { compilations.configureEach { compilerOptions.options.jvmTarget.set(JvmTarget.JVM_1_8) } }
+
+  iosArm64()
+  iosX64()
+  iosSimulatorArm64()
   linuxX64()
+  macosX64()
+  macosArm64()
+  mingwX64()
+
   sourceSets {
     val commonMain by getting { dependencies { implementation(kotlin("stdlib-common")) } }
     val commonTest by getting { dependencies { implementation(kotlin("test")) } }
-    val jvmMain by getting
-    val jvmTest by getting
-    val jsMain by getting
-    val jsTest by getting
-    val iosMain by getting
-    val iosTest by getting
-    val macosArm64Main by getting
-    val macosArm64Test by getting
-    val macosX64Main by getting
-    val macosX64Test by getting
-    val linuxArm64Main by getting
-    val linuxArm64Test by getting
-    val linuxX64Main by getting
-    val linuxX64Test by getting
     all { languageSettings.optIn("kotlin.contracts.ExperimentalContracts") }
   }
 }
