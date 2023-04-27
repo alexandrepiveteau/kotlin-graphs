3884dc354e3931a7909f32a112dd13e39e760e1b
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-27 14:03:41 +0200

    Add `erdosRenyi` builders for directed and undirected networks

--- src/commonTest/kotlin/io/github/alexandrepiveteau/graphs/builder/ErdosRenyi.kt
+++ src/commonTest/kotlin/io/github/alexandrepiveteau/graphs/builder/ErdosRenyi.kt
@@ -48,3 +48,55 @@ fun DirectedGraph.Companion.erdosRenyi(
     }
   }
 }
+
+/**
+ * Creates a random [UndirectedNetwork] with [n] vertices and an edge between each pair of vertices
+ * with probability [p].
+ *
+ * @param n the number of vertices in the graph.
+ * @param p the probability of an edge between two vertices.
+ * @param random the [Random] instance to use.
+ * @param weight the weight of each edge.
+ */
+inline fun UndirectedNetwork.Companion.erdosRenyi(
+    n: Int,
+    p: Double,
+    random: Random = Random,
+    weight: (Edge) -> Int,
+) = buildUndirectedNetwork {
+  val vertices = VertexArray(n) { addVertex() }
+  for (i in 0 until n) {
+    for (j in i + 1 until n) {
+      if (random.nextDouble() < p) {
+        val edge = vertices[i] edgeTo vertices[j]
+        addEdge(edge, weight(edge))
+      }
+    }
+  }
+}
+
+/**
+ * Creates a random [DirectedNetwork] with [n] vertices and an arc between each pair of vertices
+ * with probability [p].
+ *
+ * @param n the number of vertices in the graph.
+ * @param p the probability of an arc between two vertices.
+ * @param random the [Random] instance to use.
+ * @param weight the weight of each arc.
+ */
+inline fun DirectedNetwork.Companion.erdosRenyi(
+    n: Int,
+    p: Double,
+    random: Random = Random,
+    weight: (Arc) -> Int,
+) = buildDirectedNetwork {
+  val vertices = VertexArray(n) { addVertex() }
+  for (i in 0 until n) {
+    for (j in 0 until n) {
+      if (random.nextDouble() < p) {
+        val arc = vertices[i] arcTo vertices[j]
+        addArc(arc, weight(arc))
+      }
+    }
+  }
+}
