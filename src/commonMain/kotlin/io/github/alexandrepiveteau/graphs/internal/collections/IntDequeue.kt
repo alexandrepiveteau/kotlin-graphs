05f7bcd129dffe30eaa894b8805b02b076c6fb76
Alexandre Piveteau <alexandre@piveteau.email>
2023-04-28 19:55:59 +0200

    Document `IntDequeue` and use chunk copies in `toIntArray()`

--- src/commonMain/kotlin/io/github/alexandrepiveteau/graphs/internal/collections/IntDequeue.kt
+++ src/commonMain/kotlin/io/github/alexandrepiveteau/graphs/internal/collections/IntDequeue.kt
@@ -1,24 +1,42 @@
 package io.github.alexandrepiveteau.graphs.internal.collections
 
-/** A double-ended queue of integers. */
-// TODO : Document this.
+/** A double-ended queue of [Int]. This is a specialization of [ArrayDeque] for [Int]. */
 @PublishedApi
 internal class IntDequeue {
+
+  /** The index of the first element in the buffer. */
   private var from: Int = 0
+
+  /** The [IntArray] with the elements of the [IntDequeue]. */
   private var buffer: IntArray = IntArray(16)
+
+  /** The number of elements in the [IntDequeue]. */
   var size: Int = 0
     private set
 
+  /**
+   * Returns the first element in the [IntDequeue], or throws a [NoSuchElementException] if the
+   * [IntDequeue] is empty.
+   */
   fun peekFirst(): Int {
     if (size == 0) throw NoSuchElementException()
     return buffer[from]
   }
 
+  /**
+   * Returns the last element in the [IntDequeue], or throws a [NoSuchElementException] if the
+   * [IntDequeue] is empty.
+   */
   fun peekLast(): Int {
     if (size == 0) throw NoSuchElementException()
     return buffer[(from + size - 1).mod(buffer.size)]
   }
 
+  /**
+   * Prepends the [value] to the [IntDequeue].
+   *
+   * @param value the value to prepend.
+   */
   fun addFirst(value: Int) {
     if (size == buffer.size) {
       val newBuffer = IntArray(buffer.size * 2)
@@ -31,6 +49,11 @@ internal class IntDequeue {
     size++
   }
 
+  /**
+   * Appends the [value] to the [IntDequeue].
+   *
+   * @param value the value to append.
+   */
   fun addLast(value: Int) {
     if (size == buffer.size) {
       val newBuffer = IntArray(buffer.size * 2)
@@ -42,6 +65,10 @@ internal class IntDequeue {
     size++
   }
 
+  /**
+   * Removes and returns the first element in the [IntDequeue], or throws a [NoSuchElementException]
+   * if the [IntDequeue] is empty.
+   */
   fun removeFirst(): Int {
     val first = peekFirst()
     from = (from + 1).mod(buffer.size)
@@ -49,18 +76,23 @@ internal class IntDequeue {
     return first
   }
 
+  /**
+   * Removes and returns the last element in the [IntDequeue], or throws a [NoSuchElementException]
+   * if the [IntDequeue] is empty.
+   */
   fun removeLast(): Int {
     val last = peekLast()
     size--
     return last
   }
 
+  /** Copies the contents of the [IntDequeue] to an [IntArray], and returns it. */
   fun toIntArray(): IntArray {
     val array = IntArray(size)
-    // TODO : Chunk copy using copyInto.
-    for (i in 0 until size) {
-      array[i] = buffer[(from + i).mod(buffer.size)]
-    }
+    val end = minOf(from + size, buffer.size)
+    val len = end - from
+    buffer.copyInto(array, 0, from, end)
+    buffer.copyInto(array, len, 0, size - len)
     return array
   }
 }
